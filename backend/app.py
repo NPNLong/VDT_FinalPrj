a7833b9336cd6bf9fa52d95172e0481151d397d3
NPNLong <npnlong@gmail.com>
2025-06-22 20:25:02 +0700

    Change port

--- backend/app.py
+++ backend/app.py
@@ -162,4 +162,4 @@ def user_detail(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
